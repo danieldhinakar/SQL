a1deb2e90f9e1fff12ad60e8ff1e70ae92f4be65
Daniel Dhinakar <57731811+danieldhinakar@users.noreply.github.com>
2026-01-31 17:05:07 +0530

    Update contacts.sql

--- contacts.sql
+++ contacts.sql
@@ -6,5 +6,22 @@ USE Addressbook;
 CREATE DATABASE Addressbook;
 
 
+CREATE TABLE Contacts (
+    ContactID INT PRIMARY KEY AUTO_INCREMENT,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    PhoneNumber VARCHAR(20),
+    Email VARCHAR(100),
+    Address VARCHAR(255),
+    City VARCHAR(50),
+    State VARCHAR(50),
+    ZipCode VARCHAR(10),
+    Country VARCHAR(50),
+    Notes TEXT
+);
 
 
+INSERT INTO Contacts 
+(FirstName, LastName, PhoneNumber, Email, Address, City, State, ZipCode, Country, Notes)
+VALUES 
+('Daniel', 'Dhinakar', '+91-9014549884', 'danieldhinakar@outlook.in', 'Guntur', 'Vinukonda', 'AP', '522647', 'India', 'DevOps');
